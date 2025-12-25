b0c21a4eca8a89e5923fd2de0804177c5195a4bc
WahajHassan883 <wahajhassan007@gmail.com>
2025-12-25 17:09:54 +0500

    feat: add and configure vite-plugin-sitemap for sitemap generation

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import sitemapPlugin from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemapPlugin({
+      hostname: "https://lordevs.com",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
